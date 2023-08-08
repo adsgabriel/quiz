faa1ff0b4052c26463a3956e57f6c7af13801083
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2023-08-08 19:47:44 -0300

    Criando os componentes base

---
+++ src/components/Questao.tsx
@@ -0,0 +1,38 @@
+import styles from '../styles/Questao.module.css'
+import QuestaoModel from "@/model/questao";
+import Enunciado from './Enunciado';
+import Resposta from './Resposta';
+
+const letras = [
+    { valor: 'A', cor: '#F2C866'},
+    { valor: 'B', cor: '#F266BA'},
+    { valor: 'C', cor: '#85D4F2'},
+    { valor: 'D', cor: '#BCE596'},
+]
+
+interface QuestaoProps {
+    valor: QuestaoModel
+}
+
+export default function Questao(props: QuestaoProps) {
+    const questao = props.valor
+
+    function renderizarRespostas() {
+        return questao.respostas.map((resposta, i) => {
+            return <Resposta 
+                key={i}
+                valor={resposta}
+                indice={i}
+                letra={letras[i].valor}
+                corFundoLetra={letras[i].cor}
+            ></Resposta>
+        })
+    }
+
+    return (
+        <div className={styles.questao}>
+            <Enunciado texto={questao.enunciado}></Enunciado>
+            {renderizarRespostas()}
+        </div>
+    )
+}
---
+++ src/components/Resposta.tsx
@@ -0,0 +1,30 @@
+import styles from '../styles/Resposta.module.css'
+import RespostaModel from "@/model/resposta"
+
+interface RespostaProps {
+    valor: RespostaModel
+    indice: number
+    letra: string
+    corFundoLetra: string 
+}
+
+export default function Resposta(props: RespostaProps ) {
+    const resposta = props.valor
+    return (
+        <div className={styles.resposta}>
+            <div className={styles.conteudoResposta}>
+                <div className={styles.frente}>
+                    <div className={styles.letra} style={{ backgroundColor: props.corFundoLetra}}>
+                        {props.letra}
+                    </div>
+                    <div className={styles.valor}>
+                        {resposta.valor}
+                    </div>
+                </div>
+                <div className={styles.verso}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
