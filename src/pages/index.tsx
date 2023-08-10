e69696720640e890b815b9bc2af8dac16e72aae0
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2023-08-10 06:55:10 -0300

    Ajustes componentes questao e resposta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 import Questao from '@/components/Questao'
 import QuestaoModel from '@/model/questao'
 import RespostaModel from '@/model/resposta'
+import { useState } from 'react'
+
+const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
+  RespostaModel.errada('Verde'),
+  RespostaModel.errada('Vermelha'),
+  RespostaModel.errada('Azul'),
+  RespostaModel.certa('Preta'),
+])
 
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
-    RespostaModel.errada('Verde'),
-    RespostaModel.errada('Vermelha'),
-    RespostaModel.errada('Azul'),
-    RespostaModel.certa('Preta'),
-  ])
   
+  const [questao, setQuestao] = useState(questaoMock)
+  
+  function respostaFornecida(indice: number) {
+    setQuestao(questao.responderCom(indice))
+  }
+
   return (
     <div style={{
       display: 'flex',
@@ -18,7 +26,8 @@ export default function Home() {
       alignItems: 'center'
 
     }}>
-      <Questao valor={questaoTeste}></Questao>
+      <Questao valor={questao} 
+      respostaFornecida={respostaFornecida}></Questao>
     </div>
   )
 }
